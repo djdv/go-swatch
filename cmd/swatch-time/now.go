49af3f6c19795d7cb891a0cc599f15dd25f64aad
Dominic Della Valle <ddvpublic@gmail.com>
2023-02-15 02:20:52 -0500

    swatch-time: Don't print full path of cmd's name

--- cmd/swatch-time/now.go
+++ cmd/swatch-time/now.go
@@ -5,27 +5,38 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/djdv/go-swatch"
 )
 
 func main() {
-	standard := flag.Bool("s", false, "use Swatch standard format @000")
-	raw := flag.Bool("r", false, "use raw float format @000.000000")
+	var (
+		execName      = filepath.Base(os.Args[0])
+		cmdName       = strings.TrimSuffix(execName, filepath.Ext(execName))
+		flagSet       = flag.NewFlagSet(cmdName, flag.ExitOnError)
+		raw, standard bool
+	)
+	flagSet.BoolVar(&raw, "r", false, "use raw float format @000.000000")
+	flagSet.BoolVar(&standard, "s", false, "use Swatch standard format @000")
 
-	flag.Parse()
+	if flagSet.Parse(os.Args[1:]) != nil {
+		return
+	}
 
-	if *raw && *standard {
+	if raw && standard {
 		fmt.Println("Use none or 1 command flags, never both")
 		return
 	}
 
-	if *raw {
+	if raw {
 		fmt.Println(swatch.Now(swatch.Raw))
 		return
 	}
 
-	if *standard {
+	if standard {
 		fmt.Println(swatch.Now(swatch.Swatch))
 		return
 	}
