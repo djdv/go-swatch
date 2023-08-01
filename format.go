7106c23c48f75bb6f5d162d007db75cefba5d00a
Dominic Della Valle <ddvpublic@gmail.com>
2023-08-01 08:10:24 -0400

    use `strings.Replacer` in `Format` + const fmt
    Same effect with less function/method calls.

--- format.go
+++ format.go
@@ -7,21 +7,19 @@ import (
 )
 
 func (t *internetTime) Format(layout string) string {
-	// There's no "@" in time.Format src so it's safe to use as a delimeter
-	// Replace in decending order of precision
-	layout = t.Time.Format(layout)
-
-	layout = strings.Replace(layout, Micro.String(), t.format(Micro), 1)
-	layout = strings.Replace(layout, Mili.String(), t.format(Mili), 1)
-	layout = strings.Replace(layout, Centi.String(), t.format(Centi), 1)
-	layout = strings.Replace(layout, Deci.String(), t.format(Deci), 1)
-	layout = strings.Replace(layout, Swatch.String(), t.format(Swatch), 1)
-
-	return layout
+	// There's no "@" in time.Format src so it's safe to use as a delimiter
+	// Replace in descending order of precision
+	return strings.NewReplacer(
+		microFormat, t.format(Micro),
+		miliFormat, t.format(Mili),
+		centiFormat, t.format(Centi),
+		deciFormat, t.format(Deci),
+		swatchFormat, t.format(Swatch),
+	).Replace(t.Time.Format(layout))
 }
 
 func (t *internetTime) String() string {
-	return t.Format(Swatch.String())
+	return t.Format(swatchFormat)
 }
 
 // Expects layout to only be
